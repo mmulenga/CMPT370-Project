1b2ffb0272eb0c2d69ccca83df155b7d7b5737fd
cchesheng <cchesheng@gmail.com>
2017-10-12 15:23:03 -0600

    Add files via upload
    Initializing LoginView, loginController and application class.

---
+++ Volunteerize/src/LoginView.java
@@ -0,0 +1,61 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package volunteerize;
+
+
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.StackPane;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+
+
+/**
+ *
+ * @author irene
+ */
+public class LoginView extends Pane{
+    GridPane root; 
+    
+    LoginView(){
+        root = new GridPane();   
+        Text scenetitle = new Text("Welcome to Volunteerize!");
+
+        Label userName = new Label("Username:");
+        Label pw = new Label("Password:");
+        TextField userNameField = new TextField();
+        PasswordField pwField = new PasswordField();
+        
+        Button login = new Button("Login");
+        Button Signup = new Button("Sign Up");
+        
+        root.setAlignment(Pos.CENTER);
+        root.setHgap(10);
+        root.setVgap(10);
+        root.setPadding(new Insets(25,25,25,25));
+        
+        root.add(scenetitle,0,0,2,1);
+        root.add(userName,0,1);
+        root.add(pw,0,2);
+        root.add(userNameField,1,1);
+        root.add(pwField,1,2);
+        root.add(login, 1,3);
+        root.add(Signup, 1,4);
+       
+    }
+    
+}
+    
---
+++ Volunteerize/src/Volunteerize.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package volunteerize;
+
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.StackPane;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author irene
+ */
+public class Volunteerize extends Application {
+    
+    @Override
+    public void start(Stage primaryStage) {
+        LoginView lginView = new LoginView();
+        
+        Scene scene = new Scene(lginView.root, 400, 350);
+        
+        primaryStage.setTitle("Volunteerize");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+}
