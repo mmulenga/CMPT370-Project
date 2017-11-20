b07ad55217e5193e1731f96bd2d89ab888da82aa
sarahveddy <sarahvedress@gmail.com>
2017-11-20 17:05:26 -0600

    ProfileCheck class commented out for now. Compiles and runs

--- src/com/teamb/model/ProfileCheck.java
+++ src/com/teamb/model/ProfileCheck.java
@@ -9,49 +9,49 @@ import javafx.beans.property.BooleanProperty;
 
 //Class for using profiles with checkboxes.
 public class ProfileCheck{
-    Profile profile;
-    private BooleanProperty active;
-    BooleanProperty wantsEmails;
-    BooleanProperty wantsPhoneCalls;
-
-
-    public ProfileCheck(Profile p){
-        profile = p;
-
-        wantsEmails.setValue(p.ContactByEmail());
-        wantsPhoneCalls.setValue(p.ContactByPhone());
-    }
-
-    public BooleanProperty isActive(){
-        return active;
-    }
-
-    public void SetActive(boolean b){
-        active.setValue(b); ;
-    }
-
-    public void SetProfile(Profile p){
-        profile = p;
-    }
-
-    public String GetName(){
-        //TODO: This may change when the guys are done
-        return profile.GetName();
-    }
-
-    public Profile GetProfile(){
-        return profile;
-    }
-
-    public BooleanProperty WantsEmails(){
-
-        return wantsEmails;
-    }
-
-    public BooleanProperty WantsPhoneCalls(){
-        return  wantsPhoneCalls;
-    }
-
-    //TODO Change bools to boolean property
+//    Profile profile;
+//    private BooleanProperty active;
+//    BooleanProperty wantsEmails;
+//    BooleanProperty wantsPhoneCalls;
+//
+//
+//    public ProfileCheck(Profile p){
+//        profile = p;
+//
+//        wantsEmails.setValue(p.ContactByEmail());
+//        wantsPhoneCalls.setValue(p.ContactByPhone());
+//    }
+//
+//    public BooleanProperty isActive(){
+//        return active;
+//    }
+//
+//    public void SetActive(boolean b){
+//        active.setValue(b); ;
+//    }
+//
+//    public void SetProfile(Profile p){
+//        profile = p;
+//    }
+//
+//    public String GetName(){
+//        //TODO: This may change when the guys are done
+//        return profile.GetName();
+//    }
+//
+//    public Profile GetProfile(){
+//        return profile;
+//    }
+//
+//    public BooleanProperty WantsEmails(){
+//
+//        return wantsEmails;
+//    }
+//
+//    public BooleanProperty WantsPhoneCalls(){
+//        return  wantsPhoneCalls;
+//    }
+//
+//    //TODO Change bools to boolean property
 
 }
