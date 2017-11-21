20e92cddb4c1681c3f09d6e9e54aaf3141e54ca2
sarahveddy <sarahvedress@gmail.com>
2017-11-21 15:46:38 -0600

    controller and view

--- src/com/teamb/controller/ManageVolunteersController.java
+++ src/com/teamb/controller/ManageVolunteersController.java
@@ -1,18 +1,10 @@
 package com.teamb.controller;
 
-import com.teamb.model.Availability;
-import com.teamb.model.Profile;
-import com.teamb.model.ProfileCheck;
-import com.teamb.model.VolunteerGroup;
+
 import com.teamb.view.BasicView;
 import com.teamb.view.ManageVolunteersView;
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableArray;
-import javafx.collections.ObservableList;
 import javafx.stage.Stage;
 
-import java.util.ArrayList;
-import java.util.Observable;
 
 /**
  * Created by Sarah on 2017-11-19.
@@ -20,46 +12,17 @@ import java.util.Observable;
 public class ManageVolunteersController extends BasicController{
 
 
-//    ArrayList<Profile> allProfiles;
-//    ArrayList<Profile> filteredProfiles;
-    ObservableList<ProfileCheck> observableProfiles;
 
     ManageVolunteersView view;
 
     public ManageVolunteersController(Stage s) {
         super(s);
-        observableProfiles = CreateDummyList();
         view = new ManageVolunteersView(this);
     }
 
     @Override
     protected BasicView GetView() {
         return view;
-    }
-
-
-
-    public ObservableList<ProfileCheck> CreateDummyList(){
-        //TODO: Create observable list of ProfileChecks from a list of Profiles instead
-        ObservableList<ProfileCheck> dummyList = FXCollections.observableArrayList();
-
-        for (int i = 0; i < 10; i++) {
-           Profile profile = new Profile();
-           profile.setAllBaseInformation("Firstname" + i, "LastName" + i, "Address" + i,
-                   "phone" + i, "Emergancy Contact #" + i,
-                   "Emergancy Contact Name" + i, "Email" + i, true,
-                   true,  i, true,
-                   "Medical Info" + i, i, "PhotoPath" + i,
-                   new Availability("availability", true, true, true, true, true, true, true));
-           ProfileCheck pc = new ProfileCheck(profile);
-           pc.SetActive(false);
-           dummyList.add(pc);
-        }
-        return dummyList;
-    }
-
-    public ObservableList<ProfileCheck> GetObservableProfiles(){
-        return observableProfiles;
     }
 
 }
--- src/com/teamb/view/ManageVolunteersView.java
+++ src/com/teamb/view/ManageVolunteersView.java
@@ -1,22 +1,7 @@
 package com.teamb.view;
 
-import com.teamb.controller.BasicController;
-import com.teamb.controller.MainLandingController;
 import com.teamb.controller.ManageVolunteersController;
-import com.teamb.model.Event;
-import com.teamb.model.Profile;
-import com.teamb.model.ProfileCheck;
-import javafx.beans.property.BooleanProperty;
-import javafx.beans.property.SimpleBooleanProperty;
-import javafx.collections.ObservableList;
-import javafx.geometry.Insets;
-import javafx.geometry.Pos;
-import javafx.scene.control.Button;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
-import javafx.scene.control.cell.CheckBoxTableCell;
-import javafx.scene.control.cell.PropertyValueFactory;
-import javafx.scene.layout.BorderPane;
+
 import javafx.scene.layout.Pane;
 
 
@@ -39,14 +24,8 @@ public class ManageVolunteersView extends BasicView {
     public ManageVolunteersView(ManageVolunteersController c) {
         super(c);
         controller = c;
-        CreateChildren();
     }
 
-    private <S,T> TableColumn<S,T> createColumn(String title, String propertyName) {
-        TableColumn<S,T> column = new TableColumn<>(title);
-        column.setCellValueFactory(new PropertyValueFactory<S, T>(propertyName));
-        return column;
-    }
 
     /**
      * Returns the root pane.
@@ -63,84 +42,7 @@ public class ManageVolunteersView extends BasicView {
      */
     @Override
     protected void CreateChildren() {
-        //Create the table
-
-        TableView<ProfileCheck> table = new TableView<>();
-
-        ObservableList<ProfileCheck> dummylist = ((ManageVolunteersController) controller).GetObservableProfiles();//TODO this will be the actual list
-
-        table.setItems(dummylist);
-
-        TableColumn<ProfileCheck,String> firstNameCol = new TableColumn<ProfileCheck,String>("First Name");
-        firstNameCol.setCellValueFactory(new PropertyValueFactory("firstName"));
-
-        for (ProfileCheck p : dummylist) {
-            p.isActive().addListener((obs, old, now) -> {
-                System.out.print(obs);
-
-            });
-        }
-
-//        TableColumn<ProfileCheck, String> nameCol = new TableColumn<>("Name");
-//        TableColumn<ProfileCheck, Boolean> phoneCol = new TableColumn<>("Phone?");
-//        TableColumn<ProfileCheck, Boolean> emailCol = new TableColumn<>("Email?");
-//        TableColumn<ProfileCheck, Boolean> activeCol = new TableColumn<>("Select");
-
-        //Create observable list
 
 
-
-            //System.out.println(controller.toString());
-
-            //table.getColumns().addAll(nameCol, phoneCol, emailCol, activeCol);
-
-            table.setEditable(true);
-
-//            phoneCol.setCellValueFactory(new PropertyValueFactory<>("wantsPhoneCall"));
-//            phoneCol.setVisible(true);
-//
-//            emailCol.setCellValueFactory(new PropertyValueFactory<>("wantsEmails"));
-//            emailCol.setVisible(true);
-//
-//            activeCol.setCellFactory(CheckBoxTableCell.forTableColumn(activeCol));
-//            activeCol.setVisible(true);
-//            activeCol.setEditable(true);
-            root.getChildren().add(table);
     }
-
-
-
-//        TableView<ProfileCheck> table = new TableView<>();
-//        table.setEditable(true);
-//        table.getColumns().add(createColumn("Name", "name"));
-//
-//        TableColumn<ProfileCheck, Boolean> activeCol = createColumn("Active", "active");
-//        table.getColumns().add(activeCol);
-//
-//        activeCol.setCellFactory(col -> {
-//            CheckBoxTableCell<ProfileCheck, Boolean> cell = new CheckBoxTableCell<>(index -> {
-//                BooleanProperty active = new SimpleBooleanProperty(table.getItems().get(index).isActive());
-//                active.addListener((obs, wasActive, isNowActive) -> {
-//                    ProfileCheck profileCheck = table.getItems().get(index);
-//                    profileCheck.SetActive(isNowActive);
-//                });
-//                return active ;
-//            });
-//            return cell ;
-//        });
-//        //Create the button
-//        //TODO for testing - delete this after
-//        Button listActiveButton = new Button("List active");
-//        listActiveButton.setOnAction(e ->
-//                table.getItems().stream()
-//                        .filter(ProfileCheck::isActive)
-//                        .map(ProfileCheck::GetName)
-//                        .forEach(System.out::println));
-//
-//        BorderPane mainContainer = new BorderPane(table, null, null, listActiveButton, null) ;
-//        BorderPane.setAlignment(listActiveButton, Pos.CENTER);
-//        BorderPane.setMargin(listActiveButton, new Insets(10));
-
-        //root.getChildren().add(mainContainer);
-    //}
 }
