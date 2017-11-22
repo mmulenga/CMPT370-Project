934f919fd92b8693a0e3996823f3e7ec81c28ac7
officemahn <32247261+officemahn@users.noreply.github.com>
2017-11-21 22:47:48 -0600

    Fixed Create Event Controller

--- src/com/teamb/controller/CreateEventController.java
+++ src/com/teamb/controller/CreateEventController.java
@@ -44,19 +44,6 @@ public class CreateEventController extends BasicController {
         }
     }
 
-    class clearEventHandler implements EventHandler<ActionEvent> {
-        @Override
-        public void handle(ActionEvent event) {
-            view.eventTitleField.clear();
-            view.locationField.clear();
-            view.descriptionArea.clear();
-            view.startTimeBox.getSelectionModel().select(0);
-            view.endTimeBox.getSelectionModel().select(0);
-            view.startDatePicker.setValue(LocalDate.now());
-            view.endDatePicker.setValue(view.startDatePicker.getValue().plusDays(1));
-        }
-
-    }
 
     class homeEventHandler implements EventHandler<ActionEvent> {
         @Override
@@ -78,15 +65,6 @@ public class CreateEventController extends BasicController {
             view.endDatePicker.setValue(view.startDatePicker.getValue().plusDays(1));
         }
     }
-
-    class submitEventHandler implements EventHandler<ActionEvent>{
-        @Override
-        public void handle(ActionEvent event) {
-            createNewEvent();
-            completePopUP();
-        }
-    }
-
 
 
     @Override
